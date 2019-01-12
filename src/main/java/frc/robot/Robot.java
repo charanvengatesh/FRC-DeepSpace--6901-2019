d811250659c8da54313021dbe66a8e20a422acb9
joshz793 <45646197+joshz793@users.noreply.github.com>
2019-01-12 15:46:10 -0600

    Added code to stream from camera

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,6 +17,16 @@ import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.Victor;
 //import edu.wpi.first.wpilibj.Ultrasonic;
+//Camera imports
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -37,6 +47,7 @@ public class Robot extends TimedRobot {
   public static Victor intake3;
   
   //Sensors and variables:
+  Thread m_visionThread;
   //public static Ultrasonic  ultra;
   public double intakePower; 
 
@@ -63,8 +74,45 @@ public class Robot extends TimedRobot {
     //ultrasonic initialization:
     //ultra = new Ultrasonic(1,1); 
     //ultra.setAutomaticMode(true);
+    m_visionThread = new Thread(() -> {
+      // Get the UsbCamera from CameraServer
+      UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+      // Set the resolution
+      camera.setResolution(640, 480);
 
+      
+      // Get a CvSink. This will capture Mats from the camera
+      CvSink cvSink = CameraServer.getInstance().getVideo();
+      // Setup a CvSource. This will send images back to the Dashboard
+      CvSource outputStream
+          = CameraServer.getInstance().putVideo("Rectangle", 640, 480);
+
+      // Mats are very memory expensive. Lets reuse this Mat.
+      Mat mat = new Mat();
+
+      // This cannot be 'true'. The program will never exit if it is. This
+      // lets the robot stop this thread when restarting robot code or
+      // deploying.
+      while (!Thread.interrupted()) {
+        // Tell the CvSink to grab a frame from the camera and put it
+        // in the source mat.  If there is an error notify the output.
+        if (cvSink.grabFrame(mat) == 0) {
+          // Send the output the error.
+          outputStream.notifyError(cvSink.getError());
+          // skip the rest of the current iteration
+          continue;
+        }
+        // Put a rectangle on the image
+          //Imgproc.rectangle(mat, new Point(100, 100), new Point(400, 400),
+          //    new Scalar(255, 255, 255), 5);
+        // Give the output stream a new image to display
+        outputStream.putFrame(mat);
+      }
+    });
+    m_visionThread.setDaemon(true);
+    m_visionThread.start();
   }
+
 
   /**
    * This function is called every robot packet, no matter the mode. Use
@@ -146,7 +194,7 @@ public class Robot extends TimedRobot {
     //test to see if you can run programs on the robot:
     //m_driveTrain.tankDrive(.5,.5);
     //test drive train code with controller
-    m_driveTrain.arcadeDrive(m_oi.controller.getY(Left),m_oi.controller.getX(Right));
+    m_driveTrain.tankDrive(m_oi.controller.getY(Right),m_oi.controller.getY(Left));
     
     //uncomment lines 147-149 to test intake motors here
     //intakePower = 0.5; //DO NOT SET OVER .6!!!
